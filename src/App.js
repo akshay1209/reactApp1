9b26823639e2c66c4a05ce3693ec302adacaa515
Akshay Kumar <akshay.120995@gmail.com>
2021-11-13 20:09:00 +0530

    1st

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends Component {
+   state = { msg: false
+   }
+   fun=()=>{
+     this.setState({msg:true});
+   };
+render() {
+  
+  return(
+    <div>
+    {this.state.msg && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+    <button id="click" onClick={this.fun}>click</button>
+     </div>
   );
+}
 }
 
 export default App;
